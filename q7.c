b813ead1528ab8f10a07a8fce12368b18f88c9ad
Lucas Rocha Rangel <91744541+Lucsesen@users.noreply.github.com>
2025-07-09 12:37:36 -0300

    Update q7.c

--- q7.c
+++ q7.c
@@ -5,7 +5,7 @@
 
 #define TAM 50 // tamanho do vetor
 
-// Função para preencher o vetor com números aleatórios de 1 a 100
+// preenchendo o vetor com numeros aleatórios de 1 a 100
 void preencherVetor(int vetor[TAM]) {
     srand(time(NULL)); // inicializa gerador aleatório
     for (int i = 0; i < TAM; i++) {
@@ -19,24 +19,24 @@ void imprimirVetor(int vetor[TAM]) {
     for (int i = 0; i < TAM; i++) {
         printf("%3d ", vetor[i]);
         if ((i + 1) % 10 == 0) {
-            printf("\n"); // quebra de linha a cada 10 números
+            printf("\n");
         }
     }
 }
 
 // Função recursiva para encontrar o maior elemento do vetor
 int encontrarMaior(int vetor[TAM], int indice, int maiorAtual) {
-    // Caso base: percorreu todos os elementos
+    // Se tiver percorrido todos os elementos, retorna o maior
     if (indice == TAM) {
         return maiorAtual;
     }
 
-    // Atualiza o maior valor se necessário
+    // Atualizar o maior valor caso for necessário
     if (vetor[indice] > maiorAtual) {
         maiorAtual = vetor[indice];
     }
 
-    // Chamada recursiva para o próximo elemento
+    // Chamadada recursiva para o prox elemento
     return encontrarMaior(vetor, indice + 1, maiorAtual);
 }
 
@@ -52,7 +52,6 @@ int main() {
     // Encontrar o maior valor usando recursão
     int maior = encontrarMaior(vetor, 0, vetor[0]);
 
-    // Imprimir o maior valor encontrado
     printf("\nMaior valor do vetor: %d\n", maior);
 
     return 0;
